1678d561a5e7edc3a558f7b1f72674f160f66b03
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-10 00:22:05 +0900

    functionを追加

---
+++ functions.py
@@ -0,0 +1,22 @@
+def say_hello(greeting):
+    print(greeting)
+
+
+hello = say_hello
+hello("Good Morning")
+
+
+def add(num01, num02):
+    return(num01 + num02)
+
+
+print(6 + 2)
+
+
+def addAve(a, b, c):
+    return((a + b + c) / 3)
+
+
+add_result = addAve(9, 4, 2)
+
+print(add_result)
