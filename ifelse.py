bf891aa743728ad65e46d8a91975ee11d09ccfb9
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-12 23:54:28 +0900

    1日の挨拶を追加

--- ifelse.py
+++ ifelse.py
@@ -20,3 +20,14 @@ elif yourAge == 0:
     print("baby")
 else:
     print("child")
+
+hour = 10
+
+if hour <= 6:
+    print('so sleepy')
+elif 10 <= hour <= 12:
+    print('good morning')
+elif hour <= 12 and day_of_week == "sunday" or hour > 20:
+    print('Good afternoon')
+else:
+    print('Hi')
