1cf0b172f5b014e2b50b55649b48587a82c532eb
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-13 22:10:16 +0900

    loopの形の学習

--- ifelse.py
+++ ifelse.py
@@ -31,3 +31,19 @@ elif hour <= 12 and day_of_week == "sunday" or hour > 20:
     print('Good afternoon')
 else:
     print('Hi')
+
+# indent style
+
+
+def api():
+    search_no = get_search_no()
+    if not search_no:
+        Logger.get().warn("search_no doesn't exist.")
+        return None, "error"
+    else:
+        try:
+            result = special_execute_something(search_no)
+            return result, None
+        except:
+            Logger.get().exeption("Oh my goooood!!!!!")
+            print(search_no)
--- loopsFor.py
+++ loopsFor.py
@@ -1,6 +1,9 @@
 for i in range(5):
     print(i)
 
+for i in range(10):
+    print(i)
+
 # break
 for i in range(5):
     if i == 3:
@@ -14,3 +17,19 @@ for i in range(5):
         continue
     print(i)
 # display output goes to '0124'.
+
+# endless loop
+# myLife = 5
+# while myLife:
+#     myLife -= 4
+#     print(myLife)
+
+for item in ["a", "b", "c"]:
+    print(item)
+
+idx = 0
+while True:
+    if idx > 5:
+        break
+    idx += 1
+    print(idx)
